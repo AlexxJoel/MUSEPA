e52e8856e38c5b4cb17a1eb44d528d17a38af93a
PabloSFlores <i20213tn013@utez.edu.mx>
2024-06-07 14:00:47 -0600

    [ADD] update event

--- crud_events/update_event.py
+++ crud_events/update_event.py
@@ -1,45 +1,52 @@
-import psycopg2
 import json
-from utils.database import get_db_connection
+import logging
+
+import psycopg2
+
+from crud_events.utils.database import get_db_connection
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
+# Función de controlador Lambda
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    data = update_event(body)
-    if data is None:
-        # Handle error case (e.g., log or return specific error message)
+    try:
+        body = json.loads(event['body'])
+        data = update_event(body)
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": data,
+            }),
+        }
+    except Exception as e:
+        logger.error(f"Error updating events: {e}")
         return {
             "statusCode": 500,
-            "body": json.dumps({"message": "Error update events."})
+            "body": json.dumps({"message": "Error updating events."})
         }
 
-    # Success case: include retrieved events in the body
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": data,
-        }),
-    }
 
+# Función para actualizar eventos
 def update_event(event):
     id_event = event.get('id')
     name = event.get('name')
     description = event.get('description')
     start_date = event.get('start_date')
     end_date = event.get('end_date')
-    category = event.get('event')
+    category = event.get('category')
     pictures = event.get('pictures')
+    id_museum = event.get('id_museum')
     try:
         conn = get_db_connection()
         with conn.cursor() as cursor:
-            sql = """UPDATE events SET name=%s,description=%s,start_date=%s,end_date=%s,category=%s,pictures=%s WHERE id =%s"""
-            cursor.execute(sql,(name,description,start_date,end_date,category,pictures,id_event))
-            return "Update event successfully."
+            sql = """UPDATE events SET name=%s, description=%s, start_date=%s, end_date=%s, category=%s, pictures=%s, id_museum=%s WHERE id=%s"""
+            cursor.execute(sql, (name, description, start_date, end_date, category, pictures, id_museum, id_event))
+            conn.commit()
+            return "Event updated successfully."
     except psycopg2.Error as e:
-        print(e)
-        raise RuntimeError("ERROR EN ACTUALIZAR")
+        logger.error(f"Error updating event: {e}")
+        return "Error updating event."
     finally:
         conn.close()
-
-
-
