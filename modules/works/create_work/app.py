641bd11c983f53245af16ab76263f93249b6f67d
AlejandroMorellano <20213tn026@utez.edu.mx>
2024-06-08 15:20:59 -0600

    [ADD] Work CRUD

---
+++ modules/works/create_work/app.py
@@ -0,0 +1,71 @@
+import json
+import psycopg2
+from functions import (datetime_serializer, serialize_rows)
+
+def lambda_handler(event, __):
+    try:
+
+        # Conexión a la base de datos
+        conn = psycopg2.connect(
+            host='ep-gentle-mode-a4hjun6w-pooler.us-east-1.aws.neon.tech',
+            user = 'default',
+            password = 'pnQI1h7sNfFK',
+            database = 'verceldb'
+        )
+
+        # check if the connection is successful
+        if conn is None:
+            return {
+                'statusCode': 500,
+                'body': json.dumps("Connection to the database failed")
+            }
+
+        # Check if the event has a body
+        if 'body' not in event:
+            return {
+                'statusCode': 400,
+                'body': json.dumps("No body provided")
+            }
+
+        # Try to load the JSON body from the event
+        try:
+            request_body = json.loads(event['body'])
+        except json.JSONDecodeError:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"error": "The request body is not valid JSON"})
+            }
+
+        # todo: validate the request body
+
+        request_body = json.loads(event['body'])
+
+        title = request_body['title']
+        description = request_body['description']
+        creation_date = request_body['creation_date']
+        technique = request_body['technique']
+        artists = request_body['artists']
+        id_museum = request_body['id_museum']
+        pictures = request_body['pictures']
+
+        cur = conn.cursor()
+
+        # execute the query
+        sql = """INSERT INTO works(title, description, creation_date, technique, artists, id_museum, pictures) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+        cur.execute(sql, (title, description, creation_date, technique, artists, id_museum, pictures))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return  {
+            'statusCode': 200,
+            'body': json.dumps("Work created successfully")
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
---
+++ modules/works/delete_work/app.py
@@ -0,0 +1,47 @@
+import json
+import psycopg2
+from functions import (datetime_serializer, serialize_rows)
+
+def lambda_handler(event, context):
+    try:
+        # Conexión a la base de datos
+        conn = psycopg2.connect(
+            host='ep-gentle-mode-a4hjun6w-pooler.us-east-1.aws.neon.tech',
+            user = 'default',
+            password = 'pnQI1h7sNfFK',
+            database = 'verceldb'
+        )
+
+        if not conn:
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"error": "Failed to connect to the database"})
+            }
+
+        if event['pathParameters'] is None or 'id' not in event['pathParameters']:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"error": "Request ID is missing from the request body"})
+            }
+
+
+        cur = conn.cursor()
+
+        request_id = event['pathParameters']['id']
+        sql = """DELETE FROM works  WHERE id =%s"""
+        cur.execute(sql, (request_id,))
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return  {
+            'statusCode': 200,
+            'body': json.dumps({"message": "Work deleted successfully"})
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
