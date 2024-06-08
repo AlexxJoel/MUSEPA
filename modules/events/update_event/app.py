b3be510cae87ecf61302433e9471cdc008de414f
alexxjoel <20213tn019@utez.edu.mx>
2024-06-08 04:47:54 -0600

    [ADD] Events

--- modules/events/update_event/app.py
+++ modules/events/update_event/app.py
@@ -63,7 +63,7 @@ def lambda_handler(event, __):
 
         return {
             'statusCode': 200,
-            'body': json.dumps({"message": "Event updated successfully!"})
+            'body': json.dumps({"message": "Event updated successfully yeah!"})
         }
 
     except Exception as e:
