babeac26b8d6577bbcd982972da50f7d345bd21c
alexxjoel <20213tn019@utez.edu.mx>
2024-06-08 02:21:26 -0600

    [ADD] Get events

--- crud_events/app.py
+++
@@ -1,4 +0,0 @@
-from get_event import lambda_handler as get_event
-from delete_event import lambda_handler as delete_event
-from insert_event import lambda_handler as insert_event
-from update_event import lambda_handler as update_event
