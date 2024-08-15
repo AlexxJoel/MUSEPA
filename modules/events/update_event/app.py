078e56ef163826118e9e19810d33809744622b5c
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-08-15 10:29:11 -0600

    [FIX] app authorizate and delete yml

--- modules/events/update_event/app.py
+++ modules/events/update_event/app.py
@@ -191,7 +191,7 @@ def img_to_base64(img):
 
 def authorizate_user(_event):
     token = _event['headers']['Authorization'].split(' ')[1]
-    decoded_token = jwt.decode(token, options={"verify_signature": False})
+    decoded_token = jwt.decode(token, key="secret", algorithms="HS256")
     roles = decoded_token.get('cognito:groups')
     role = roles[0]
 
--- modules/works/delete_work/app.py
+++ modules/works/delete_work/app.py
@@ -72,7 +72,7 @@ def lambda_handler(event, _context):
 # -------------------AUTHORIZATION----------------------------
 def authorizate_user(_event):
     token = _event['headers']['Authorization'].split(' ')[1]
-    decoded_token = jwt.decode(token, options={"verify_signature": False})
+    decoded_token = jwt.decode(token, key="secret", algorithms="HS256")
     roles = decoded_token.get('cognito:groups')
     role = roles[0]
 
